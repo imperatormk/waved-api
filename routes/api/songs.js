ba9eb893663ec53c772d053763a5bf1e8f4428a9
Darko Simonovski <darko.simonovski@hotmail.com>
2019-06-16 16:53:54 +0200

    Added initial song insert endpoint Moved some endpoints

---
+++ routes/api/songs.js
@@ -0,0 +1,50 @@
+const router = require('express').Router()
+
+const fs = require('fs')
+const db = require(__basedir + '/db/controllers')
+
+var multer  = require('multer')
+var upload = multer({ dest: 'files/' })
+
+router.get('/:id', (req, res) => {
+  const songId = req.params.id
+  const pitch = req.query.pitch || 0
+
+  return db.songs.getSongById(songId)
+    .then((result) => {
+      if (!result) return res.status(404).send({ msg: 'invalidSong' })
+      const song = result.toJSON()
+
+      const pitchedTracks = song.tracks.map((track) => {
+        return {
+          ...track,
+          url: `track.url_${pitch}`
+        }
+      })
+      return res.send({
+        ...song,
+        tracks: pitchedTracks
+      })
+    })
+})
+
+router.get('/:id/tracks/:audioName', (req, res) => {
+  const audioName = req.params.audioName.trim()
+  const audioPath = `${__basedir}/files/${audioName}.mp3`
+
+  fs.access(audioPath, fs.F_OK, (err) => {
+    if (err) {
+      return res.status(404).send({ msg: 'invalidTrack' })
+    }
+    return res.sendFile(audioPath)
+  })
+})
+
+const uploadMw = upload.fields([{ name: 'tracks', maxCount: 10 }, { name: 'data' }])
+router.post('/', uploadMw, (req, res) => {
+  const data = JSON.parse(req.body.data)
+  const { song, tracks } = data
+  res.send({ song, tracks })
+})
+
+module.exports = router
