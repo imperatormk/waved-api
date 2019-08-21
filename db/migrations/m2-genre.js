a8ffa3b15c36fc868c6fbb5f97bda5c334d1679a
Darko <darko.simonovski@bestisoft.com>
2019-08-21 15:28:27 +0200

    A fix for existing songs with null genre

--- db/migrations/m2-genre.js
+++ db/migrations/m2-genre.js
@@ -8,7 +8,8 @@ module.exports = {
         'genre',
         {
           type: Sequelize.STRING,
-          allowNull: false
+          allowNull: false,
+          defaultValue: 'rock'
         }
       )
     ])
