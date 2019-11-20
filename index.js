2098ab272ec21683e1ca1bbf8b6fd861f498c73c
Darko Simonovski <darko.simonovski@hotmail.com>
2019-11-20 05:14:38 +0100

    Make server HTTPS

--- index.js
+++ index.js
@@ -2,14 +2,27 @@ global.__basedir = __dirname
 const dotenv = require('dotenv')
 dotenv.config()
 
-const http = require('http')
+const fs = require('fs')
+const https = require('https')
 const app = require('./app')
+
+const key = fs.readFileSync('/etc/letsencrypt/live/studiodoblo.de/privkey.pem')
+const cert = fs.readFileSync('/etc/letsencrypt/live/studiodoblo.de/cert.pem')
+const ca = fs.readFileSync('/etc/letsencrypt/live/studiodoblo.de/chain.pem')
+
+const sslOpts = {
+  key,
+  cert,
+  ca: [ca],
+  requestCert: false,
+  rejectUnauthorized: false
+}
 
 const port = process.env.PORT || 3000
 app.set('port', port)
 
 require(__basedir + '/db')
 
-const server = http.createServer(app)
+const server = https.createServer(sslOpts, app)
 server.listen(port)
 console.log('Started on port ' + port)
