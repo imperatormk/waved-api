bb3ddb4b649ca15c4c85dcf85a19dca8e216a96b
Darko Simonovski <darko.simonovski@hotmail.com>
2019-11-22 19:28:11 +0100

    An oopsie

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 const fs = require('fs')
-const https = require('http')
+const https = require('https')
 const app = require('./app')
 
 const key = fs.readFileSync('/etc/letsencrypt/live/studiodoblo.de/privkey.pem')
