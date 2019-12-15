ed78d056c426c70f09f1a52ef87a5a3a64d024eb
Darko Simonovski <darko.simonovski@hotmail.com>
2019-12-15 17:42:31 +0100

    Implement user update endpoint Introduce changes to adapt in the accounts service

--- routes/api/accounts.js
+++ routes/api/accounts.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 
 const db = require(__basedir + '/db/controllers')
-const { register } = require(__basedir + '/services/accounts')
+const { register, update } = require(__basedir + '/services/accounts')
 const { authMiddleware } = require(__basedir + '/services/auth')
 
 router.post('/register', (req, res, next) => {
@@ -11,10 +11,29 @@ router.post('/register', (req, res, next) => {
     .catch(err => next(err))
 })
 
+router.post('/update', authMiddleware, (req, res, next) => {
+  const userId = req.user.id
+  const {
+    email, password, confirmPassword
+  } = req.body
+  const userObj = {
+    email, password, confirmPassword
+  }
+
+  return db.users.getUserById(userId) // use as template for avoiding DRY
+    .then((user) => {
+      if (!user) throw { status: 404, msg: 'userNotFound' }
+
+      userObj.username = user.username
+      return update(userObj, userId)
+        .then((result) => res.json(result))
+    })
+    .catch(err => next(err))
+})
+
 router.get('/:id', authMiddleware, (req, res, next) => {
   const userId = req.params.id
-
-  return db.users.getUserById(userId) // DRY!
+  return db.users.getUserById(userId) // use as template for avoiding DRY
     .then((user) => {
       if (!user) throw { status: 404, msg: 'userNotFound' }
       return res.send(user)
--- services/accounts/index.js
+++ services/accounts/index.js
@@ -1,15 +1,19 @@
 const exportsObj = {}
 
+const Op = require('sequelize').Op
 const db = require(__basedir + '/db/controllers')
+
 const mailerService = require(__basedir + '/services/mailer')
 const helper = require('./helper')
 
-const sanitizeUser = (user) => {
+const sanitizeUser = (user, passwordRequired) => {
   const passwordFields = ['password', 'confirmPassword']
-  const requiredFields = ['username', 'email', ...passwordFields]
+  const requiredFields = ['username', 'email']
+  if (passwordRequired) requiredFields.push(...passwordFields)
+
   const badFields = []
   const sanitizedUser = {}
-
+  
   if (!user) return null
 
   requiredFields.forEach((field) => {
@@ -18,19 +22,22 @@ const sanitizeUser = (user) => {
     if (!value) badFields.push(field)
   })
 
-  const passwordsMatch = sanitizedUser.password === sanitizedUser.confirmPassword
-  const isPasswordEmpty = !!badFields.filter(field => passwordFields.includes(field)).length
-  if (!passwordsMatch && !isPasswordEmpty) badFields.push(...passwordFields)
+  if (passwordRequired) {
+    const passwordsMatch = sanitizedUser.password === sanitizedUser.confirmPassword
+    const isPasswordEmpty = !!badFields.filter(field => passwordFields.includes(field)).length
+    if (!passwordsMatch && !isPasswordEmpty) badFields.push(...passwordFields)
+  }
 
   if (badFields.length) return { badFields }
   delete sanitizedUser.confirmPassword
   return { user: sanitizedUser }
 }
 
-const checkDuplicateValues = (user) => {
+const checkDuplicateValues = (user, userId) => {
   const duplicateValues = ['username', 'email'].map((field) => {
     const criteria = {}
     criteria[field] = user[field]
+    if (userId) criteria.id = { [Op.not]: userId }
     return db.users.getUser(criteria)
       .then(result => ({ exists: !!result, field }))
   })
@@ -41,14 +48,14 @@ const checkDuplicateValues = (user) => {
 }
 
 exportsObj.register = (user) => {
-  const sanitizationResult = sanitizeUser(user)
+  const sanitizationResult = sanitizeUser(user, true)
   if (!sanitizationResult) 
     return Promise.reject({ status: 400, msg: 'emptyUser' })
   if (sanitizationResult.badFields) 
     return Promise.reject({ status: 400, msg: 'invalidFields', details: sanitizationResult.badFields })
 
   const sanitizedUser = sanitizationResult.user
-  return checkDuplicateValues(sanitizedUser)
+  return checkDuplicateValues(sanitizedUser, null)
     .then((duplicateValues) => {
       if (duplicateValues.length)
         return Promise.reject({ status: 409, msg: 'duplicateFields', details: duplicateValues })
@@ -60,6 +67,30 @@ exportsObj.register = (user) => {
           mailerService.sendRegisterConfirmationEmail(email, { username })
           return user
         })
+    })
+}
+
+exportsObj.update = (user, userId) => {
+  const hasPassword = !!user.password
+  const sanitizationResult = sanitizeUser(user, hasPassword)
+  if (!sanitizationResult) 
+    return Promise.reject({ status: 400, msg: 'emptyUser' })
+  if (sanitizationResult.badFields) 
+    return Promise.reject({ status: 400, msg: 'invalidFields', details: sanitizationResult.badFields })
+
+  const sanitizedUser = sanitizationResult.user
+  return checkDuplicateValues(sanitizedUser, userId)
+    .then((duplicateValues) => {
+      if (duplicateValues.length)
+        return Promise.reject({ status: 409, msg: 'duplicateFields', details: duplicateValues })
+
+      user.id = userId
+      if (hasPassword) {
+        return helper.hashPassword(sanitizedUser.password)
+          .then(hashedPassword => ({ ...user, password: hashedPassword }))
+          .then(user => db.users.updateUser(user))
+      }
+      return db.users.updateUser(user)
     })
 }
 
